5aa89a3b0675e70ece12b2f79b0a2bd3a7c45f25
TarunPatidar04 <tarun.coderfarm@gmail.com>
2024-06-10 22:18:11 +0530

    WIP create a Project  section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technology from "./components/Technology";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <About />
         <Technology />
         <Experience />
+        <Projects/>
       </div>
     </div>
   );
